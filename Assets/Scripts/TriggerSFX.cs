ad639b376cba0a033a454a418c9733b155814072
JacobFYeomans <90638405+JacobFYeomans@users.noreply.github.com>
2021-11-26 10:24:02 -0400

    Initial commit

---
+++ Assets/Scripts/TriggerSFX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSFX : MonoBehaviour
+{
+
+    public AudioSource source;
+    public GameObject trigger;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        source.Play();
+        trigger.SetActive(false);
+    }
+}
